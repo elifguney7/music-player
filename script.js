98ea3a6dd98db2581f509c0b2c277d0ecbdca2b4
elifguney7 <elifguney13@gmail.com>
2023-09-03 23:57:12 +0300

    Dynamic progress bar functionality.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const image = document.querySelector('img');
 const title = document.getElementById('title');
 const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
@@ -87,6 +89,18 @@ function prevSong() {
 // On load select first Song
 loadSong(songs[songIndex]);
 
+// Update progress bar & time
+
+function updateProgressBar(e) {
+if (isPlay) {
+    const { duration, currentTime } = e.srcElement;
+    // update progress bar
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width=`${progressPercent}%`;
+}
+}
+
 // Event listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
